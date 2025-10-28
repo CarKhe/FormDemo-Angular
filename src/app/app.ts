9090de29ef42daf2d2fee1b651915a9a6019445d
Carlos Rodriguez <carloskiller177@gmail.com>
2025-10-28 18:41:26 -0500

    inicio Formulario

---
+++ src/app/app.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterOutlet } from '@angular/router';
+import { GenericInput } from './shared/components/generic-input/generic-input';
+import { GenericButton } from './shared/components/generic-button/generic-button';
+import { GenericSelect } from "./shared/components/generic-select/generic-select";
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule, GenericInput, GenericButton, GenericSelect],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'FormDemo';
+  formulario: FormGroup;
+
+  roles = [
+    { id: 1, nombre: 'Administrador' },
+    { id: 2, nombre: 'Usuario' },
+    { id: 3, nombre: 'Invitado' }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      nombre: [''],
+      correo: [''],
+      telefono: [''],
+      rol:['']
+    });
+  }
+
+  
+
+  guardar() {
+    const resultado = this.formulario.value;
+    console.log('JSON result:', resultado);
+    alert('Datos en JSON:\n' + JSON.stringify(resultado, null, 2));
+  }
+}
---
+++ src/app/shared/components/generic-button/generic-button.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-generic-button',
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  templateUrl: './generic-button.html',
+  styleUrl: './generic-button.scss',
+})
+export class GenericButton {
+  @Input() label: string = '';
+  @Input() color: 'primary' | 'accent' | 'warn' | undefined;
+  @Input() type:
+    | 'basic'
+    | 'elevated'
+    | 'outlined'
+    | 'filled'
+    | 'tonal'
+    | 'icon'
+    | 'fab'
+    | 'mini-fab'
+    | 'fab-extended' = 'basic';
+  @Input() icon?: string;
+  @Input() disabled: boolean = false;
+
+  @Output() click = new EventEmitter<void>();
+
+  onClick() {
+    this.click.emit();
+  }
+}
